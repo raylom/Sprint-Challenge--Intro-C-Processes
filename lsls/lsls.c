1b0e901f05fc8fb6def7dce7e6ff2554fbe075f3
raylom <rlom96@gmail.com>
2019-06-07 15:49:52 -0500

    started program

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -7,12 +7,23 @@
 int main(int argc, char **argv)
 {
   // Parse command line
-
+      DIR *d;
+    struct dirent *dir;
   // Open directory
-
+    d = opendir(".");
   // Repeatly read and print entries
 
-  // Close directory
+   if (d)
+    {
+        while ((dir = readdir(d)) != NULL)
+        {
+            printf("%s\n", dir->d_name);
+        }
+        // Close directory
+        closedir(d);
+    }
+
+  
 
   return 0;
 }
